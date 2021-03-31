d0d5dc0c7e8f888338a6f7684c856d0ab0a53245
Jorge Marcos Chávez <jormarchav@gmail.com>
2021-03-31 19:14:42 +0200

    Add entities for Firebase

---
+++ UIFragmentsJorge/app/src/main/java/com/uc3m/uifragments/api/Recipe.kt
@@ -0,0 +1,28 @@
+package com.uc3m.uifragments.api
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Recipe (
+    @SerializedName("id")
+    @Expose
+    val id: String = "",
+    @SerializedName("user")
+    @Expose
+    val user: String = "",
+    @SerializedName("name")
+    @Expose
+    val name: String = "",
+    @SerializedName("description")
+    @Expose
+    val description: String = "",
+    @SerializedName("time")
+    @Expose
+    val time: Float = 0.0f,
+    @SerializedName("ingredients")
+    @Expose
+    val ingredients: Map<String, String>,
+    @SerializedName("steps")
+    @Expose
+    val steps: Map<Int, String>
+)
---
+++ UIFragmentsJorge/app/src/main/java/com/uc3m/uifragments/api/User.kt
@@ -0,0 +1,19 @@
+package com.uc3m.uifragments.api
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class User (
+    @SerializedName("name")
+    @Expose
+    val name: String = "",
+    @SerializedName("surname")
+    @Expose
+    val surname: Map<Int, String>,
+    @SerializedName("email")
+    @Expose
+    val email: String = "",
+    @SerializedName("password")
+    @Expose
+    val password: String = ""
+)
