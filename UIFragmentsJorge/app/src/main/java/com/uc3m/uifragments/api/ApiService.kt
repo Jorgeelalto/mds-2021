c4a9cd1b4782aad696c49a2ebf4db6681ff6283d
Jorge Marcos <jormarchav@gmail.com>
2021-04-03 10:38:25 +0200

    Added more API calls

--- UIFragmentsJorge/app/src/main/java/com/uc3m/uifragments/api/ApiService.kt
+++ UIFragmentsJorge/app/src/main/java/com/uc3m/uifragments/api/ApiService.kt
@@ -1,19 +1,28 @@
 package com.uc3m.uifragments.api
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ApiService {
-    @GET("/users/{id}.json")
-    fun getUser(@Path(value = "name") name: Int): Call<User>
-
     @GET("/users.json")
     fun getAllUsers(): Call<List<User>>
+
+    @GET("/users/{name}.json")
+    fun getUser(@Path(value = "name") name: String): Call<User>
+
+    @POST("/users/{name}.json")
+    fun postUser(@Path(value = "name") name: String, @Body user: User): Call<User>
+
+
+    @GET("recipes.json")
+    fun getAllRecipes(): Call<List<Recipe>>
 
     @GET("/recipes/{id}.json")
     fun getRecipe(@Path(value = "id") id: String): Call<Recipe>
 
-    @GET("recipes.json")
-    fun getAllRecipes(): Call<List<Recipe>>
+    @POST("/recipes/{id}.json")
+    fun postRecipe(@Path(value = "id") id: String, @Body recipe: Recipe): Call<Recipe>
 }
