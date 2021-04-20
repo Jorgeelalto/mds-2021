34efd3ed94a51ed35e54a19adffb8115e45b9a1f
Jorge Marcos <jormarchav@gmail.com>
2021-04-20 20:22:33 +0200

    API cosas

--- UIFragmentsJorge/app/src/main/java/com/uc3m/foodstuff/api/ApiRecipeRecyclerAdapter.kt
+++ UIFragmentsJorge/app/src/main/java/com/uc3m/foodstuff/api/ApiRecipeRecyclerAdapter.kt
@@ -1,12 +1,18 @@
 package com.uc3m.foodstuff.api
 
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.uc3m.foodstuff.R
+import java.net.URL
+
+const val TAG = "ApiRecipeRecyclerAdapt"
 
 class ApiRecipeRecyclerAdapter(
         val context: Context,
@@ -29,11 +35,21 @@ class ApiRecipeRecyclerAdapter(
     inner class Holder(view: View?) : RecyclerView.ViewHolder(view!!) {
         private val recipeName = view?.findViewById<TextView>(R.id.recipe_name)
         private val recipeTime = view?.findViewById<TextView>(R.id.recipe_time)
+        private val recipeImg = view?.findViewById<ImageView>(R.id.recipe_img)
 
         fun bind(recipe: ApiRecipe, context: Context) {
             // TODO make nice
             recipeName?.text = recipe.title
             recipeTime?.text = recipe.publisher
+
+            Log.d(TAG, recipe.image_url)
+            try {
+                val bmp = BitmapFactory.decodeStream(URL(recipe.image_url).openStream())
+                recipeImg?.setImageBitmap(bmp)
+            } catch (t: Throwable) {
+                Log.d(TAG, t.localizedMessage)
+                Log.d(TAG, t.stackTrace.toString())
+            }
         }
     }
 }
